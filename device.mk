c9f905779bc08e51efa00dfd4fe9d48d59d1e071
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-05-26 03:22:08 +0200

    jflteusc: Add mising overlays
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jflteusc/jflteusc-vendor.mk)
 
+## device overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/jflteusc/overlay
+
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay-cdma
 
